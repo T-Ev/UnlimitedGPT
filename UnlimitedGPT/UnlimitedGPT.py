16ae19863bc2c54e60c08e0ce4f6a7cc8cb9e126
T-Ev <tjerobotics@gmail.com>
2024-09-26 16:10:26 -0400

    dfgd

--- UnlimitedGPT/UnlimitedGPT.py
+++ UnlimitedGPT/UnlimitedGPT.py
@@ -439,7 +439,7 @@ class ChatGPT:
         self.logger.debug(f"Prev length: {len(logs_raw)}")
         self.logger.debug("Refreshing the page to get the latest conversations...")
         # self.driver.refresh()
-        sleep(2)  # Wait for 2 seconds to ensure the page is fully loaded
+        # sleep(2)  # Wait for 2 seconds to ensure the page is fully loaded
         logs_raw = self.driver.get_log("performance")
         self.logger.debug(f"Refeshed length: {len(logs_raw)}")
         
